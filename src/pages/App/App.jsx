935422fe2fa7191d4e492deb2708db12ca473870
towhid046 <tamzidislam788@gmail.com>
2023-12-05 09:19:09 -0600

    [DONE]

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,15 @@
+import Todo from './../../Components/TodoApp/Todo';
+
+const App = () => {
+  return(
+    <>
+    <div>
+      <>
+      <Todo/>
+      </>
+    </div>
+    </>
+  )
+}
+
+export default App;
